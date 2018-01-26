e3c9f1569895bd6ac520e6b297901957dabf6a38
William Pettersson <william.pettersson@glasgow.ac.uk>
2018-01-26 10:25:37 +0000

    Use the new features

--- params.py
+++ params.py
@@ -2,18 +2,18 @@
 
 import sys
 import os
-from kidney_utils.readers import read_xml, read_json
+from kidney_utils.readers import read_instance
 
+
+print("name,size,edge_count,density,diameter,radius")
 for filename in sys.argv[1:]:
     filename = filename.rstrip()
-    graph = None
-    if filename[-4:] == ".xml":
-        graph = read_xml(filename)
-    elif filename[-5:] == ".json":
-        graph = read_json(filename)
+    graph = read_instance(filename)
     if graph:
         density = graph.density()
         diameter = graph.diameter()
         radius = graph.radius()
+        size = graph.size()
+        edges = graph.edge_count()
         name = os.path.basename(filename)
-        print("%s,%.3f,%.3f,%.3f" % (name, density, diameter, radius))
+        print("%s,%s,%s,%.3f,%.3f,%.3f" % (name, size, edges, density, diameter, radius))
